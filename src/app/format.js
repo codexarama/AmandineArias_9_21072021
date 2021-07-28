cf9641c0e76828f7186ced0ef80f575992c58bbf
Amandine Arias <amandine.arias@gmail.com>
2021-07-28 13:46:32 +0200

    project initialisation

---
+++ src/app/format.js
@@ -0,0 +1,22 @@
+export const formatDate = (dateStr) => {
+  const date = new Date(dateStr)
+  const ye = new Intl.DateTimeFormat('fr', { year: 'numeric' }).format(date)
+  const mo = new Intl.DateTimeFormat('fr', { month: 'short' }).format(date)
+  const da = new Intl.DateTimeFormat('fr', { day: '2-digit' }).format(date)
+  const month = mo.charAt(0).toUpperCase() + mo.slice(1)
+  // return `${parseInt(da)} ${month.substr(0,3)}. ${ye.toString().substr(2,4)}` // original
+  return `${parseInt(da)} ${month.substr(0,3)}. ${ye.toString().substr(2,3)}` // ne fonctionne pas
+  // return `${parseInt(da)} ${month.substr(0,3)}. ${parseInt(ye)}` // ne fonctionne pas
+}
+
+export const formatStatus = (status) => {
+  switch (status) {
+    case "pending":
+      return "En attente"
+    case "accepted":
+      return "Accepté"
+    case "refused":
+      return "Refusé"
+      // return "Refused" // ORIGINAL
+  }
+}
