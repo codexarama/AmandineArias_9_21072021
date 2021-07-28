19291d94286aea66f09aa80d2256c27730240065
Amandine Arias <amandine.arias@gmail.com>
2021-07-28 15:07:44 +0200

    delete com

--- src/views/NewBillUI.js
+++ src/views/NewBillUI.js
@@ -55,9 +55,7 @@ export default () => {
                   <div class="col-half">
                     <label for="file" class="bold-label">Justificatif</label>
                     <input required type="file" class="form-control blue-border" data-testid="file" />
-                    // added
                     <div class="error-extensionFile">Votre justificatif doit être une image de format (.jpg) ou (.jpeg) ou (.png)</div>
-                    //
                   </div>
                 </div>
             </div>
