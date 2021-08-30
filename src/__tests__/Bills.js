5e2d317539e773e98a467d3ae50df1732b7b6ac5
Amandine Arias <amandine.arias@gmail.com>
2021-08-30 18:14:43 +0200

    fix : sort bills in decending order

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,50 +1,103 @@
 import { screen } from '@testing-library/dom';
 import { bills } from '../fixtures/bills.js';
-import { ROUTES } from '../constants/routes';
-import { localStorageMock } from '../__mocks__/localStorage';
 import BillsUI from '../views/BillsUI.js';
-import Bills from '../containers/Bills';
-import userEvent from '@testing-library/user-event';
 
 describe('Given I am connected as an employee', () => {
   describe('When I am on Bills Page and there are no bill', () => {
-    test('Then bills should be empty', () => {
+    test('Then bills should render an empty table', () => {
       const html = BillsUI({ data: [] });
       document.body.innerHTML = html;
-      //to-do write expect expression // DONE
-      const eyeIconElt = screen.queryByTestId('icon-eye');
-      expect(eyeIconElt).toBeNull();
+      // to-do write expect expression
+      // DONE :
+      const table = screen.queryByTestId('table');
+      expect(table.innerHTML).toBe('');
     });
+  });
+
+  // test loading page sur BillUI
+  test('Then, Loading page should be rendered', () => {
+    // BillUI.loading, condition qui retourne la loading page si TRUE
+    const html = BillsUI({ loading: true });
+    document.body.innerHTML = html;
+    // Attend une valeur retour du DOM, verifie que le contenu est TRUE
+    expect(screen.getAllByText('Loading...')).toBeTruthy();
   });
 
   describe('When I am on Bills Page and there are bill(s)', () => {
     test('Then bills should be ordered from earliest to latest', () => {
-      const html = BillsUI({ data: bills });
-      document.body.innerHTML = html;
-      // TestingLibraryElementError: Unable to find an element with the text:
-      // /^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/i.
-      // This could bebecause the text is broken up by multiple elements.
-      // In this case, you can provide a function for your text matcher to make your matcher more flexible
-      // TESTS -------------------------------------------------------------------------------------------
-      // const dates = screen.getAllByText("21.02.21").map(a => a.innerHTML)
-      // const dates = screen.getAllByText(/^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/i).map(a => a.innerHTML)
+      const frenchMonths = [];
+      for (let i = 0; i < 12; i++) {
+        frenchMonths.push(
+          new Intl.DateTimeFormat('fr', { month: 'short' }).format(
+            new Date(2000, i)
+          )
+        );
+      }
 
-      // // ORIGINAL ----------------------------------------------------------------------------------------------------------------
-      // const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
+      // reverse Us date (year, month, day) into Fr date (day, month, year)
+      const formatDateReverse = (formatedDate) => {
+        let [day, month, year] = formatedDate.split(' ');
+        day = parseInt(day);
+        month = frenchMonths.findIndex(
+          (element) => element === month.toLowerCase()
+        );
+        year =
+          parseInt(year) < 70 ? 2000 + parseInt(year) : 1900 + parseInt(year); //arbitrary range for year : 1970-2069
+        return new Date(year, month, day);
+      };
 
-      // recupere les dates
+      document.body.innerHTML = BillsUI({ data: billsSample });
       const dates = Array.from(
         document.body.querySelectorAll('#data-table tbody>tr>td:nth-child(3)')
       ).map((a) => a.innerHTML);
-
-      console.log(dates);
-
-      const antiChrono = (a, b) => (a < b ? 1 : -1);
-      const datesSorted = [...dates].sort(antiChrono);
-      console.log(datesSorted);
-
+      const antiChronoSort = (a, b) =>
+        formatDateReverse(a) < formatDateReverse(b) ? 1 : -1;
+      const datesSorted = [...dates].sort(antiChronoSort);
       expect(dates).toEqual(datesSorted);
-      // screen.debug();
     });
   });
+
+  /* ORIGINAL ----------------------------------------------------------------------------------------------------------------
+      const dates = screen
+        .getAllByText(
+          /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i
+        )
+        .map((a) => a.innerHTML);
+
+      // recupere les dates
+      expect(dates).toEqual([...dates].sort((a, b) => (a < b ? 1 : -1)));
+    });
+  });*/
 });
+
+// describe('When I am on Bills page and back-end send an error message', () => {
+//   test('Then, Error page should be rendered', () => {
+//     document.body.innerHTML = BillsUI({ error: 'some error message' });
+//     expect(screen.getAllByText('Erreur')).toBeTruthy();
+//   });
+// });
+
+// describe('When I am on Bills page and I click on the iconEye of one of the bills', () => {
+//   test('Then, the open modal handler should be run', () => {
+//     document.body.innerHTML = BillsUI({ data: bills });
+//     const sampleBills = new Bills({
+//       document,
+//       onNavigate,
+//       firestore: null,
+//       localStorage: window.localStorage,
+//     });
+//     sampleBills.handleClickIconEye = jest.fn();
+//     screen.getAllByTestId('icon-eye')[0].click();
+//     expect(sampleBills.handleClickIconEye).toBeCalled();
+//   });
+
+//   test("Then, the modal should display the attached image", () => {
+//     document.body.innerHTML = BillsUI({ data: bills })
+//     const sampleBills = new Bills({ document, onNavigate, firestore: null, localStorage: window.localStorage })
+//     const iconEye = document.querySelector(`div[data-testid="icon-eye"]`)
+//     $.fn.modal = jest.fn()
+//     sampleBills.handleClickIconEye(iconEye)
+//     expect($.fn.modal).toBeCalled()
+//     expect(document.querySelector(".modal")).toBeTruthy()
+//   })
+// });
--- src/app/format.js
+++ src/app/format.js
@@ -1,4 +1,5 @@
-export const formatDate = (dateStr) => {
+/* ORIGINAL ------------------------------
+  export const formatDate = (dateStr) => {
   const date = new Date(dateStr)
   const ye = new Intl.DateTimeFormat('fr', { year: 'numeric' }).format(date)
   // convert string into number for month value (more simple)
@@ -6,16 +7,31 @@ export const formatDate = (dateStr) => {
   const da = new Intl.DateTimeFormat('fr', { day: '2-digit' }).format(date)
   // display right french date format
   return `${(da)}.${mo}.${ye.toString().substr(2,4)}`
-}
+}*/
+
+
+export const formatDate = (dateStr) => {
+
+  // invalid Dates (day-month-year instead of year-month-day) in the DB makes the bill's page to crash
+  // fix : manage formatDate Us / Fr
+  if (Date.parse(dateStr) === NaN || dateStr === '') return '1 Jan. 01';
+
+  const date = new Date(dateStr);
+  const ye = new Intl.DateTimeFormat('fr', { year: 'numeric' }).format(date);
+  const mo = new Intl.DateTimeFormat('fr', { month: 'short' }).format(date);
+  const da = new Intl.DateTimeFormat('fr', { day: '2-digit' }).format(date);
+  const month = mo.charAt(0).toUpperCase() + mo.slice(1);
+  return `${parseInt(da)} ${month.substr(0, 3)}. ${ye.toString().substr(2, 4)}`;
+};
 
 export const formatStatus = (status) => {
   switch (status) {
-    case "pending":
-      return "En attente"
-    case "accepted":
-      return "Accepté"
-    case "refused":
-      // in french like others
-      return "Refusé"
+    case 'pending':
+      return 'En attente';
+    case 'accepted':
+      return 'Accepté';
+    case 'refused':
+      // in french like the others
+      return 'Refusé';
   }
-}
+};
