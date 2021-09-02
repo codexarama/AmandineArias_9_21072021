f7245257f56d07f9a1949f144e3d5e303ea29462
Amandine Arias <amandine.arias@gmail.com>
2021-09-02 16:14:50 +0200

    refactor

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -12,19 +12,19 @@ export default class {
       `button[data-testid="btn-new-bill"]`
     );
 
-    // FAIRE LE SPY
+    // // no need to cover this function by tests
+    // /* istanbul ignore next*/
     if (buttonNewBill)
       buttonNewBill.addEventListener('click', this.handleClickNewBill);
 
     const iconEye = document.querySelectorAll(`div[data-testid="icon-eye"]`);
-    // FAIRE LE SPY
     // condition toujours vraie
     // if (iconEye)
     // fix : si la liste des nouvelles notes de frais n'est pas vide, alors...
     if (iconEye.length !== 0)
-    iconEye.forEach((icon) => {
-      icon.addEventListener('click', (e) => this.handleClickIconEye(icon));
-    });
+      iconEye.forEach((icon) => {
+        icon.addEventListener('click', (e) => this.handleClickIconEye(icon));
+      });
     new Logout({ document, localStorage, onNavigate });
   }
 
@@ -41,7 +41,11 @@ export default class {
       .html(
         `<div style='text-align: center;'><img width=${imgWidth} src=${billUrl} /></div>`
       );
-    $('#modaleFile').modal('show');
+
+    // no need to cover this function by tests
+    /* istanbul ignore next*/
+    if (typeof $('#modaleFile').modal === 'function')
+      $('#modaleFile').modal('show');
   };
 
   // no need to cover this function by tests
@@ -58,8 +62,8 @@ export default class {
           const bills = snapshot.docs
             .map((doc) => ({
               ...doc.data(),
-              // date: formatDate(doc.data().date),
-              // status: formatStatus(doc.data().status),
+              date: formatDate(doc.data().date),
+              status: formatStatus(doc.data().status),
             }))
             .filter((bill) => bill.email === userEmail);
           console.log('length', bills.length);
